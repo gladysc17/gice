a8395ffd87c83597dd4dffa6306326e1d38b8a5f
gladysc17 <glacar_17@hotmail.es>
2021-05-24 17:14:34 -0500

    modelos actualizados

--- app/Models/Objetivo.php
+++ app/Models/Objetivo.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Objetivo extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'objetivo',
+    ];
 }
--- app/Models/Valor.php
+++ app/Models/Valor.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Valor extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'valor',
+    ];
 }
