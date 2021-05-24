a59bb42f3ed2c71035efa67b4c1edfeb552ac79a
gladysc17 <glacar_17@hotmail.es>
2021-05-24 17:05:47 -0500

    primer commit

---
+++ app/Http/Livewire/Misionvision.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Misionvision extends Component
+{
+    public function render()
+    {
+        return view('livewire.misionvision');
+    }
+}
